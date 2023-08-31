3c8b9d92c7280db71aee77c424abd90b12595490
Fred kibuthu <fred@kibzagency.com>
2023-09-01 00:57:11 +0300

    business features middle

--- components/Business.jsx
+++ components/Business.jsx
@@ -5,13 +5,13 @@ import { features } from '../constants'
 
 const Business = () => {
   return (
-    <section className='flex-col business_container relative'>
-        <div className='section_info'>
+    <section className='flex-col business_container flex-center gap-10 relative'>
+        <div className='section_info '>
             <h2 className='sect_head text-[24] text-center'>WE MAKE IT EASY! </h2>
             <p className='md:text-[26px] text-[24]  text-gray-600   font-normal  max-w-[540px] mt-5 '> Moreover, Company X's well-trained and compassionate drivers undergo specialized training to offer courteous assistance, making clients feel comfortable and valued throughout their travels.</p>         
            <Button />
         </div>
-        <div className="flex-col mt-8">
+        <div className=" flex-1 flex-col mt-8">
            {features.map((feature, index)=> (
             <FeatureCard key={feature.id} {...feature} index={index}/>
            ))}
