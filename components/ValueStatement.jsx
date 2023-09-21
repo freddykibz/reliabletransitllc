56bd8056679d4048bdb82802d00cfb145ee3d05d
Fred kibuthu <fred@kibzagency.com>
2023-09-22 02:22:35 +0300

    value statement fix

--- components/ValueStatement.jsx
+++ components/ValueStatement.jsx
@@ -5,7 +5,7 @@ const ValueStatement = () => {
    <section className='w-full flex-center flex-col bg-picton-gallery mt-0 md:py-6'>
      <h1 className='sect_head text-greyhood text-center py-2 '>WHY RELIABLE TRANSIT LLC</h1>
      <p className='desc_paragraphs text-greyhood md:text-center md:max-w-6xl font-normal'>
-     Network that understands the importance of being Caring, Respectful and Reliable.Our network from the personnel to partners work as a team with one mission and one goal: to make sure every service request is answered in a
+      We are a Network that understands the importance of being Caring, Respectful and Reliable.Our network from the personnel to partners work as a team with one mission and one goal: to make sure every service request is answered in a
 safe and timely manner. At Reliable Transit, we offer scalable transport
 capacity to suit your ever changing needs, so we are happy to satisfy your
 service needs.
