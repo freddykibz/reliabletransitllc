703a3e0d9717d35335088a9f48c80fad18b458e8
Fred kibuthu <fred@kibzagency.com>
2023-09-29 00:31:36 +0300

    center service perks

--- sections/ServicePerks.jsx
+++ sections/ServicePerks.jsx
@@ -13,18 +13,19 @@ const ServicePerks = () => {
         />
       </div>
      <div className="flex-1 flex-col flex-center bg-purple-600">
-     <h1 className="sect_head text-center text-picton-blue"> WE ARE </h1>
+     <h1 className="sect_head text-center text-picton-blue">WE ARE</h1>
         <div className=" w-full flex-center md:flex-start md:pl-40 flex-col gap-6 ">
-           <div className=" w-full flex gap-4 md:py-4 py-2 ">
+           <div className="w-full flex gap-4 md:py-4 py-2 flex-center">
            <Image 
                 src="assets/check.svg"
                 alt="checkmark"
                 width={32}
                 height={32}
+
             />
            <p className="list_text">Accessible</p>
            </div>
-           <div className="flex gap-4 md:py-4 py-2 ">
+           <div className="flex gap-4 md:py-4 py-2 flex-center">
            <Image 
                 src="assets/check.svg"
                 alt="checkmark"
@@ -33,32 +34,32 @@ const ServicePerks = () => {
             />
            <p className="list_text">Reliable</p>
            </div>
-           <div className="flex gap-4 md:py-4 py-2 ">
+           <div className="flex gap-4 md:py-4 py-2 flex-center ">
            <Image 
                 src="assets/check.svg"
                 alt="checkmark"
                 width={32}
                 height={32}
             />
-            <p className="list_text ">Professional</p>
+            <p className="list_text">Professional</p>
            </div>
-           <div className="flex gap-4 md:py-4 py-2">
+           <div className="flex gap-4 md:py-4 py-2 flex-center">
            <Image 
                 src="assets/check.svg"
                 alt="checkmark"
                 width={32}
                 height={32}
             />
-           <p className="list_text ">Safety Oriented </p>
+           <p className="list_text">Safety Oriented </p>
            </div>
-           <div className="flex gap-4 md:py-4 py-2"> 
+           <div className="flex gap-4 md:py-4 py-2 flex-center"> 
            <Image 
                 src="assets/check.svg"
                 alt="checkmark"
                 width={32}
                 height={32}
             />
-           <p className="list_text ">Flexible</p>
+           <p className="list_text">Flexible</p>
            </div>
           
          
